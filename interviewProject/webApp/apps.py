92b6a1cc00b7ec392c17a26a17383af3cfe47a89
Kasra <kasra76@gmail.com>
2023-05-22 14:28:00 +0330

    remove Apscheduler startup

--- interviewProject/webApp/apps.py
+++ interviewProject/webApp/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class WebappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'webApp'
-
-    def ready(self):
-        from jobs import updater
-        updater.start()
